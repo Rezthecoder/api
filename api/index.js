f447b80ccf335bd62635a8e8b2fd2a4d62a7dd5d
Rez <noobprogramming111@gmail.com>
2025-12-15 13:54:26 +0900

    done

--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
 
-import app from './index.js';     // Option A
-// import app from './api/index.js'; // Option B
-const port = 3001;
-app.listen(port, () => console.log(`Local dev: http://localhost:${port}`));
+// api/index.js
+import app from '../index.js';
+export default app
